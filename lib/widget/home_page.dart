17bd58ffa3ae3bb1d6e01831c9cd79dc6466aaf7
moomink <hhd03295@gmail.com>
2022-10-29 08:26:19 +0900

    add battery chacker

--- lib/widget/home_page.dart
+++ lib/widget/home_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:fluttertoast/fluttertoast.dart';
 import 'package:fullscreen/fullscreen.dart';
 import 'package:vibration/vibration.dart';
 import 'package:gionsai_5j/class/message_data.dart';
@@ -8,6 +9,7 @@ import 'package:provider/provider.dart';
 import 'package:socket_io_client/socket_io_client.dart' as IO;
 import 'package:audioplayers/audioplayers.dart';
 import 'package:volume_control/volume_control.dart';
+import 'package:battery_plus/battery_plus.dart';
 import 'package:kiosk_plugin/kiosk_plugin.dart';
 
 class MyHomePage extends StatefulWidget {
@@ -21,12 +23,13 @@ class MyHomePage extends StatefulWidget {
 
 class TestHomePage extends State<MyHomePage> {
   final AudioCache _player = AudioCache();
+  final _battery = Battery();
   bool _visible = false;
   bool _isClear = false;
   bool _isAction = false;
   bool _isActionEnd = false;
 
-  IO.Socket socket = IO.io('http://192.168.43.120:18526', <String, dynamic>{
+  IO.Socket socket = IO.io('http://192.168.11.10:18526', <String, dynamic>{
     'transports': ['websocket'],
     'autoConnect': false,
   });
@@ -54,6 +57,16 @@ class TestHomePage extends State<MyHomePage> {
 
     socket.on("action", (data) async {
       switch (data) {
+        case "battery":
+          int nowBattery = await Battery().batteryLevel;
+          Fluttertoast.showToast(
+              msg: "now Battery : [${nowBattery}%]",
+              gravity: ToastGravity.BOTTOM,
+              toastLength: Toast.LENGTH_LONG,
+            fontSize: 16.0
+          );
+          break;
+
         case "light":
           Utils.changeLight(enable: false);
           Utils.changeLight(enable: true);
@@ -120,54 +133,61 @@ class TestHomePage extends State<MyHomePage> {
   Widget build(BuildContext context) {
     final Size size = MediaQuery.of(context).size;
     return Scaffold(
-        body: Stack(children: <Widget>[
-      Column(crossAxisAlignment: CrossAxisAlignment.start, children: <Widget>[
-        AppBar(
+        appBar: AppBar(
           leading: const Icon(Icons.arrow_back_ios),
           centerTitle: true,
           title: const Text("♥ miyu ♥"),
         ),
-        Flexible(
-            child: StreamBuilder(
-          stream: context.watch<MessageData>().stream,
-          builder: (context, snapshot) {
-            if (snapshot.hasError) {
-              return const Text("ERROR");
-              //TODO 何とかする
-            } else if (snapshot.connectionState == ConnectionState.waiting ||
-                snapshot.connectionState == ConnectionState.waiting) {
-              return Container();
-            } else {
-              if (_isClear) {
-                var _ = snapshot.data;
-                _isClear = false;
-                return Container();
-              }
-              if (_isAction == false) {
-                if (_isActionEnd == false) {
-                  context.read<MessageData>().messages.add(snapshot.data!);
-                } else {
-                  _isActionEnd = false;
-                }
-              }
-              var widgetList = context.read<MessageData>().messages;
-              print("length:${context.read<MessageData>().messages.length}");
-              return ListView.builder(
-                  physics: BouncingScrollPhysics(),
-                  itemCount: widgetList.length,
-                  itemBuilder: (context, index) {
-                    return widgetList[index];
-                  });
-            }
-          },
-        )),
-        Image.asset("assets/chat.jpg")
-      ]),
-      Center(
-          child: Visibility(
-              visible: _visible,
-              child: FittedBox(
-                  fit: BoxFit.cover, child: Image.asset("assets/baby.gif"))))
-    ]));
+        body: Stack(children: <Widget>[
+          Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: <Widget>[
+                Flexible(
+                    child: StreamBuilder(
+                  stream: context.watch<MessageData>().stream,
+                  builder: (context, snapshot) {
+                    if (snapshot.hasError) {
+                      return const Text("ERROR");
+                      //TODO 何とかする
+                    } else if (snapshot.connectionState ==
+                        ConnectionState.waiting) {
+                      return Container();
+                    } else {
+                      if (_isClear) {
+                        var _ = snapshot.data;
+                        _isClear = false;
+                        return Container();
+                      }
+                      if (_isAction == false) {
+                        if (_isActionEnd == false) {
+                          context
+                              .read<MessageData>()
+                              .messages
+                              .add(snapshot.data!);
+                        } else {
+                          _isActionEnd = false;
+                        }
+                      }
+                      var widgetList = context.read<MessageData>().messages;
+                      print(
+                          "length:${context.read<MessageData>().messages.length}");
+                      return ListView.builder(
+                          physics: BouncingScrollPhysics(),
+                          itemCount: widgetList.length,
+                          itemBuilder: (context, index) {
+                            return widgetList[index];
+                          });
+                    }
+                  },
+                )),
+                Image.asset("assets/chat.jpg")
+              ]),
+          Center(
+              child: Visibility(
+                  visible: _visible,
+                  child: FittedBox(
+                      fit: BoxFit.cover,
+                      child: Image.asset("assets/baby.gif"))))
+        ]));
   }
 }
